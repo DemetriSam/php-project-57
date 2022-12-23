f43d31392edb725e6f95dc035f188f64e15589ad
Demetri Samartsev <samartsew@gmail.com>
2022-12-23 14:12:25 +0300

    register

--- lang/ru/auth.php
+++ lang/ru/auth.php
@@ -16,7 +16,7 @@ return [
     'failed' => 'Введите правильные имя пользователя и пароль',
     'password' => 'Введите правильные имя пользователя и пароль',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
-    'register' => 'Зарегистрироваться',
+    'register' => 'Зарегистрировать',
     'login' => 'Войти',
     'logout' => 'Выход',
     'register_button' => 'Зарегистрировать',
