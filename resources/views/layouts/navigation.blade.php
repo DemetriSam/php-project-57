ed25919a3eabd6334c0e2795b2ffbf88dcd40759
Demetri Samartsev <samartsew@gmail.com>
2022-12-23 14:21:11 +0300

    auth buttons

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -65,13 +65,14 @@
                     </x-dropdown>
                 </div>
             @else
-                <div>
-                    <a href="{{ route('login') }}"
-                        class="text-sm text-gray-700 dark:text-gray-500 underline">{{ __('auth.login') }}</a>
-
+                <div class="flex align-center gap-2 py-3">
+                    <x-primary-button>
+                        <a href="{{ route('login') }}" class="">{{ __('auth.login') }}</a>
+                    </x-primary-button>
                     @if (Route::has('register'))
-                        <a href="{{ route('register') }}"
-                            class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">{{ __('auth.register') }}</a>
+                        <x-primary-button>
+                            <a href="{{ route('register') }}" class="">{{ __('auth.register') }}</a>
+                        </x-primary-button>
                     @endif
                 </div>
             @endauth
